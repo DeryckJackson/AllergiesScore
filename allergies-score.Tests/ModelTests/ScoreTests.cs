89152413355113aa1f67a4299685e630c197450a
Tyson Lackey <lackeyt90@gmail.com>
2020-07-14 13:50:19 -0700

    red light confirm 64 test

--- allergies-score.Tests/ModelTests/ScoreTests.cs
+++ allergies-score.Tests/ModelTests/ScoreTests.cs
@@ -14,12 +14,12 @@ namespace AllergiesScore.Tests
       CollectionAssert.AreEqual(expected, testScore.ListAllergies(128));
     }
 
-    // [TestMethod]
-    // public void ListAllergies_InputNumberIncludesCatsScore_Cats()
-    // {
-    //   Score testScore = new Score();
-    //   string[] expected = {"cats"};
-    //   CollectionAssert.AreEqual(expected, testScore.ListAllergies(129));
-    // }
+    [TestMethod]
+    public void ListAllergies_InputNumberIncludesPollenScore_pollen()
+    {
+      Score testScore = new Score();
+      string[] expected = {"pollen"};
+      CollectionAssert.AreEqual(expected, testScore.ListAllergies(64));
+    }
   }
 }

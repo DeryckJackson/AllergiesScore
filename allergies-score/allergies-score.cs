45fb18e01c6a8ea1eb3d7f932e5c7a6114603b2b
Tyson Lackey <lackeyt90@gmail.com>
2020-07-14 14:21:21 -0700

    add console UI logic to capture user allergy score
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- allergies-score/allergies-score.cs
+++ allergies-score/allergies-score.cs
@@ -6,7 +6,16 @@ namespace AllergiesScore
   {
     static void Main()
     {
-      
+      Score userScore = new Score();
+      Console.WriteLine("Please Enter your Allergy Score:");
+      string userScoreString = Console.ReadLine();
+      int userScoreInt = Int32.Parse(userScoreString);
+      string[] userAllergies = userScore.ListAllergies(userScoreInt);
+      Console.WriteLine("You're Allergic to:");
+      foreach(var item in userAllergies)
+      {
+        Console.WriteLine(item.ToString());
+      }
     }
   }
 }
